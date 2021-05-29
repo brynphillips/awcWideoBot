d732e7ce0761d81a6b2145b59de767dde7725099
shazbot <17993329+brynphillips@users.noreply.github.com>
2021-05-28 21:26:11 -0700

    fixed environ

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,54 +1,61 @@
-import discord
-from bot.main import *
-from discord.ext import commands
-from io import StringIO
-import sys
+import argparse
 import os
 import re
+import sys
+from io import StringIO
 
-client = commands.Bot(command_prefix = '!')
+import discord
+from bot.main import chat_message_test
+from bot.main import Config
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
 token = os.getenv('DISCORD_BOT_TOKEN')
 config = Config(
-    "...",
-    "...",
-    "...",
-    "...",
-    os.environ['youtube_api_key'],}
-    "youtube_playlists": {
-        "explains": {
-            "playlist_id": "PLWBKAf81pmOaP9naRiNAqug6EBnkPakvY",
-            "github": "https://github.com/anthonywritescode/explains"
+    '...',
+    '...',
+    '...',
+    '...',
+    os.environ['youtube_api_key'],
+    {
+        'youtube_playlists':
+        {
+            'explains':
+                'PLWBKAf81pmOaP9naRiNAqug6EBnkPakvY',
+                'https://github.com/anthonywritescode/explains'
+            'faq':
+                'PLWBKAf81pmOZEPeIV2_pIESK5hRMAo1hR',
         },
-        "faq": {
-            "playlist_id": "PLWBKAf81pmOZEPeIV2_pIESK5hRMAo1hR"
-        },
-    "..."
-    )
-
-
+    },
+    '...',
+)
 
 
 @client.event
 async def on_ready():
     await client.change_presence(
-        status = discord.Status.idle,
-        activity = discord.Game('Listening to .help')
-        )
+        status=discord.Status.idle,
+        activity=discord.Game('Listening to .help'),
+    )
     print('Bot is ready.')
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'🏓 Pong with {str(round(client.latency, 2))}')
 
-@client.command(name = 'whoami')
+
+@client.command(name='whoami')
 async def whoami(ctx):
     await ctx.send(f'You are {ctx.message.author.name}')
+
 
 @client.command()
 async def clear(ctx, amount=3):
     await ctx.channel.purge(limit=amount)
 
-@client.command(name = 'explains')
+
+@client.command(name='explains')
 async def explains(ctx):
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.json')
@@ -61,11 +68,8 @@ async def explains(ctx):
     s = StringIO()
     sys.stdout = s
 
-    quiet = not args.verbose
-
     with open(args.config) as f:
-        config = Config(**json.load(f))
-        await chat_message_test(config,ctx.message.content)
+        await chat_message_test(config, ctx.message.content)
         sys.stdout = stdout
         s.seek(0)
         readout = s.read()
@@ -76,4 +80,3 @@ async def explains(ctx):
 
 
 client.run(token)
-
